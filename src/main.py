e9fa7a1fd6fed80774eab3dcf33e9fd6c66e8c9d
Matthew Gleich <matthewgleich@gmail.com>
2019-12-19 20:11:35 -0500

    Lower brightness to 0.1

--- src/main.py
+++ src/main.py
@@ -6,12 +6,12 @@ def main():
     while True:
         # Up Green
         for i in range(8):
-            blinkt.set_pixel(i, 0, 250, 0)
+            blinkt.set_pixel(i, 0, 250, 0, 0.1)
             blinkt.show()
             sleep(0.2)
         # Up Red
         for i in range(7, 0, -1):
-            blinkt.set_pixel(i, 250, 0, 0)
+            blinkt.set_pixel(i, 250, 0, 0, 0.1)
             blinkt.show()
             sleep(0.2)
 
