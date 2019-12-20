9c1c88b0fd074a401d82ccf4ad69ae6bf0d11554
Matthew Gleich <matthewgleich@gmail.com>
2019-12-19 19:42:40 -0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import blinkt
+from time import sleep
+
+def main():
+    blinkt.clear()
+    while True:
+        # Up Green
+        for i in range(8):
+            blinkt.set_pixel(i, 0, 250, 0)
+            blinkt.show()
+            sleep(0.2)
+        # Up Red
+        for i in range(7, 0, -1):
+            blinkt.set_pixel(i, 250, 0, 0)
+            blinkt.show()
+            sleep(0.2)
